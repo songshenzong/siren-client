5885c1ccf0f899ea75ef01d851f929f62f6f7456
Songshenzong <i@songshenzong.com>
2018-03-18 10:15:14 +0800

    support php 5.6.32

--- config/siren.php
+++ config/siren.php
@@ -2,23 +2,23 @@
 
 return [
 
-    'token' => env('SIREN_TOKEN'),
+    'token' => env('SIREN_TOKEN', ''),
 
-    'protocol' => env('SIREN_PROTOCOL'),
+    'protocol' => env('SIREN_PROTOCOL', 'udp'),
 
     'udp' => [
-        'host' => env('SIREN_UDP_HOST'),
-        'port' => env('SIREN_UDP_PORT'),
+        'host' => env('SIREN_UDP_HOST', '127.0.0.1'),
+        'port' => env('SIREN_UDP_PORT', 55656),
     ],
 
     'http' => [
-        'host' => env('SIREN_HTTP_HOST'),
-        'port' => env('SIREN_HTTP_PORT'),
+        'host' => env('SIREN_HTTP_HOST', '127.0.0.1'),
+        'port' => env('SIREN_HTTP_PORT', 55657),
     ],
 
     'tcp' => [
-        'host' => env('SIREN_TCP_HOST'),
-        'port' => env('SIREN_TCP_PORT'),
+        'host' => env('SIREN_TCP_HOST', '127.0.0.1'),
+        'port' => env('SIREN_TCP_PORT', 55658),
     ],
 
 ];
